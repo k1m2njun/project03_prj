3ba7c38cf77800917beef61b3f6f1741cc79f499
eun10 <dnsrmd99@gmail.com>
2022-11-08 18:31:09 +0900

    Post 모델 적용

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    ingredient = models.CharField(max_length=20)
+    expiration_date = models.DateTimeField()    # 유통기한
+    check = models.BooleanField(default = True) # 체크박스 활용 위해서
--- project03_prj/settings.py
+++ project03_prj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ky2k_l34e@srpo0v_qk^(6xdovzna6!c1_k#=$@n%$z@og0^hr"
+SECRET_KEY = "django-insecure-ur&rd%wx95qwofbgaojz=_qec5w185yd2t*nr&gyvlwf*jmt%q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "ingredients",
+    "single_pages",
 ]
 
 MIDDLEWARE = [
