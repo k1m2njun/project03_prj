f9c439fb3ed1c95dc1df2fd997898161ddb5bd07
Min-Jun, Kil <68175311+k1m2njun@users.noreply.github.com>
2022-11-16 18:32:03 +0900

    템플릿 연동

--- project03_prj/settings.py
+++ project03_prj/settings.py
@@ -124,7 +124,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
